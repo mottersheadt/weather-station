d4d4af49ec5930b1091da3b35587d119fd658b27
Travis Mottershead <travis@webheroes.ca>
2017-01-02 09:29:32 +0000

    Made temperatures flash when they update.

--- server/public/controllers/Temperature.js
+++ server/public/controllers/Temperature.js
@@ -3,19 +3,31 @@ app.controller('TemperatureCtrl', [
     function ($scope, $location) {
 	$scope.readableTemp = function(temp) {
 	    if(!temp)
-		return null;
+		return '...';
 	    return temp.toFixed(1);
 	}
-	
-        $scope.refresh    = metaRefresh($scope, {
+
+	$scope.updated	= false;
+        $scope.refresh	= metaRefresh($scope, {
 	    "Temperature": '/currentTemperature'
         }, function(data) {
 	    if( !data['Temperature'] ) {
 		console.log("failed to get data", data);
 		return;
 	    }
+	    var new_temp = $.extend( {}, data['Temperature'] );
+	    var old_temp = $.extend( {}, $scope.temperature );
+	    
+	    if (old_temp.celcius !== new_temp.celcius) {
+		$scope.updated = true;
+		setTimeout(function() {
+		    $scope.updated = false;
+		    $scope.$apply();
+		}, 500);
+	    }
 	    $scope.temperature    = data['Temperature'];
         });
         $scope.refresh();
+	setInterval($scope.refresh, 3000);
     }
 ]);
